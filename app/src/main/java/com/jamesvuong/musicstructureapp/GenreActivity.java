aa3740e0345011e32d5234398e09f8a5467d882c
James Vuong <jvuonger@gmail.com>
2016-09-12 16:13:30 -0400

    Song detail

--- app/src/main/java/com/jamesvuong/musicstructureapp/GenreActivity.java
+++ app/src/main/java/com/jamesvuong/musicstructureapp/GenreActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class GenreActivity extends AppCompatActivity {
 
@@ -16,5 +18,15 @@ public class GenreActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         this.setTitle(name);
+
+        //Set "Play Song" button to show detailed view
+        Button play_song_button = (Button) findViewById(R.id.play_song_button);
+        play_song_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GenreActivity.this, SongDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
