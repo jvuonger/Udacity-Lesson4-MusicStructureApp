70a055b136513a298ad05dda0ade595ca88ba2e5
James Vuong <jvuonger@gmail.com>
2016-09-12 16:18:14 -0400

    Add back button to detail

--- app/src/main/java/com/jamesvuong/musicstructureapp/SongDetailActivity.java
+++ app/src/main/java/com/jamesvuong/musicstructureapp/SongDetailActivity.java
@@ -1,7 +1,10 @@
 package com.jamesvuong.musicstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SongDetailActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SongDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_detail);
+
+        //Set "Play Song" button to show detailed view
+        Button backButton = (Button) findViewById(R.id.back_to_genre_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
